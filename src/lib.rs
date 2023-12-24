320da4419996dced562ed6ad92bf584fabf08804
Thomas Duboucher <thomas@duboucher.eu>
2023-12-24 15:08:19 +0100

    Custom symbol mangling

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 #[cfg(target_arch = "wasm32")]
 use core::panic::PanicInfo;
 
-use wasm_macro::return_as_is;
+use wasm_macro::custom_mangle;
 
 #[cfg(target_arch = "wasm32")]
 #[panic_handler]
@@ -11,14 +11,12 @@ fn panic(_panic: &PanicInfo<'_>) -> ! {
     loop {}
 }
 
-#[return_as_is]
-#[export_name = "00"]
+#[custom_mangle]
 pub fn add(a: u16, b: u16) -> u16 {
     a + b
 }
 
-#[return_as_is]
-#[export_name = "01"]
+#[custom_mangle]
 pub fn mul(a: u16, b: u16) -> u32 {
     (a * b) as u32
 }
--- wasm-macro/src/lib.rs
+++ wasm-macro/src/lib.rs
@@ -1,8 +1,24 @@
-#![no_std]
-
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, ItemFn};
 
 #[proc_macro_attribute]
-pub fn return_as_is(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    item
+pub fn custom_mangle(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let function = parse_macro_input!(item as ItemFn);
+
+    let name = &function.sig.ident;
+    let inputs = &function.sig.inputs;
+    let output = &function.sig.output;
+    let block = &function.block;
+
+    let name_token = format!("__{name}");
+
+    let expanded = quote! {
+        #[export_name = #name_token]
+        fn #name(#inputs) #output {
+            #block
+        }
+    };
+
+    TokenStream::from(expanded)
 }
