5253e658fffea2203c2805b8b3e418eac92b101d
Thomas Duboucher <thomas@duboucher.eu>
2023-12-25 22:58:29 +0100

    Use unreachable opcode

--- src/lib.rs
+++ src/lib.rs
@@ -8,15 +8,29 @@ use wasm_macro::custom_mangle;
 #[cfg(target_arch = "wasm32")]
 #[panic_handler]
 fn panic(_panic: &PanicInfo<'_>) -> ! {
-    loop {}
+    unsafe { core::hint::unreachable_unchecked() }
+}
+
+#[custom_mangle]
+pub fn install(parameters: u16) -> ! {
+    unsafe { core::hint::unreachable_unchecked() }
+}
+
+#[custom_mangle]
+pub fn process(a: u16, b: u16) -> ! {
+    unsafe { core::hint::unreachable_unchecked() }
 }
 
 #[custom_mangle]
 pub fn add(a: u16, b: u16) -> u16 {
-    a + b
+    inner(a, 0 - b)
 }
 
 #[custom_mangle]
 pub fn mul(a: u16, b: u16) -> u32 {
     (a * b) as u32
+}
+
+fn inner(a: u16, b: u16) -> u16 {
+    a - b
 }
