44c61d855be4beb67ab5c0da7d2ec34c0afd42ee
Thomas Duboucher <thomas@duboucher.eu>
2023-12-28 23:34:24 +0100

    Have fun with import, export and attributes

--- src/lib.rs
+++ src/lib.rs
@@ -2,27 +2,35 @@
 
 use wasm_macro::custom_mangle;
 
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+#[link(wasm_import_module = "mod")]
+extern {
+    #[link_name = "some_other_name"]
+    fn register();
 }
 
+#[link_section = "hello"]
+#[no_mangle]
+#[used]
+pub static SECTION: [u8; 24] = *b"This is a custom section";
+
 #[cfg(target_arch = "wasm32")]
-//#[panic_handler]
-#[allow(dead_code)]
+#[panic_handler]
+#[inline(always)]
 fn panic(_panic: &core::panic::PanicInfo<'_>) -> ! {
-    alert("");
     unsafe { core::hint::unreachable_unchecked() }
 }
 
 #[custom_mangle]
-pub fn install(_parameters: u16) -> ! {
-    unsafe { core::hint::unreachable_unchecked() }
+pub fn install(_parameters: u16) -> () {
+    unsafe { register(); }
 }
 
 #[custom_mangle]
 pub fn process(_a: u16, _b: u16) -> ! {
     unsafe { core::hint::unreachable_unchecked() }
+}
+
+#[no_mangle]
+pub fn mul(a: u16, b: u16) -> u32 {
+    (a * b) as u32
 }
--- wasm-macro/src/lib.rs
+++ wasm-macro/src/lib.rs
@@ -7,6 +7,7 @@ use syn::{parse_macro_input, ItemFn};
 pub fn custom_mangle(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let function = parse_macro_input!(item as ItemFn);
 
+    let vis = &function.vis;
     let name = &function.sig.ident;
     let inputs = &function.sig.inputs;
     let output = &function.sig.output;
@@ -15,11 +16,11 @@ pub fn custom_mangle(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let binding = Sha3_256::digest(name.to_string());
     let name_hash: &[u8] = &binding.as_slice()[..4];
     let name_hash_to_hex_string: String = hex::encode(name_hash);
-    let name_token = format!("__${name_hash_to_hex_string}");
+    let name_token = format!("__${name}${name_hash_to_hex_string}");
 
     let expanded = quote! {
         #[export_name = #name_token]
-        fn #name(#inputs) #output {
+        #vis fn #name(#inputs) #output {
             #block
         }
     };
